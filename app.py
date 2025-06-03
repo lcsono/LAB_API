25cb7e7bb82c69f96cf55e4caff7123a414b0cd7
Larissa Costa Silva <2400990@salas.aulas>
2025-06-02 22:51:39 -0300

    teste

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
 from flask_swagger_ui import get_swaggerui_blueprint
-from flask_jwt_extended import JWTManager, create_access_token,
-jwt_required
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 
 app = Flask(__name__)
 
-#Config do jwt
+# Configuração do JWT
 app.config['JWT_SECRET_KEY'] = 'your_secret_key'
 jwt = JWTManager(app)
 
-#swagger ui 
-
+# Swagger UI
 SWAGGER_URL = '/swagger'
 API_DOC_URL = '/static/swagger.json'
 swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_DOC_URL)
@@ -26,8 +24,8 @@ def get_items():
 
 @app.route('/login', methods=['POST'])
 def login():
-    access_token = create_access_token(identity = "user")
-    return jsonify (access_token = access_token)
+    access_token = create_access_token(identity="user")
+    return jsonify(access_token=access_token)
 
 @app.route('/protected', methods=['GET'])
 @jwt_required()
@@ -35,4 +33,4 @@ def protected():
     return jsonify(message="Protected route")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 1313)
+    app.run(host='0.0.0.0', port=1313)
